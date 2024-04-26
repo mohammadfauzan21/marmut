ca61dd9027220b04ef6716e5549d5d39acfd1a3f
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-04-26 08:43:55 +0700

    Initiate urlpatterns and INSTALLED_APPS

--- marmut15/marmut15/settings.py
+++ marmut15/marmut15/settings.py
@@ -31,6 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'preboarding',
+    'login',
+    'register',
+    'dashboardreguser',
+    'dashboardartist',
+    'dashboardsongwriter',
+    'dashboardlabel',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- marmut15/marmut15/urls.py
+++ marmut15/marmut15/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('preboarding.urls')),
+    path('login/', include('login.urls')),
+    path('register/', include('register.urls')),
+    path('user/dashboard/', include('dashboarduser.urls')),
+    path('artist/dashboard/', include('dashboardartist.urls')),
+    path('songwriter/dashboard/', include('dashboardsongwriter.urls')),
+    path('label/dashboard/', include('dashboardlabel.urls')),
 ]
