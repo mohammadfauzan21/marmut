094bda9e87f177adb464f743735166292c3385d8
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-04-26 08:35:10 +0700

    Start app IA

---
+++ marmut15/dashboardpodcaster/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DashboardpodcasterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashboardpodcaster'
---
+++ marmut15/dashboardsongwriter/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DashboardsongwriterConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dashboardsongwriter'
