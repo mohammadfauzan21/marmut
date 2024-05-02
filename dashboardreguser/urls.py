a994eeeb61ef1a4579369c04ce5ec890c5d796ef
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-05-02 23:01:13 +0700

    chart

--- dashboardreguser/urls.py
+++ dashboardreguser/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dashboardreguser.views import dashboarduser
+from dashboardreguser.views import dashboarduser,chart
 
 app_name = 'dashboardreguser'
 urlpatterns = [
     path('', dashboarduser, name='dashboarduser'),
+    path('chart', chart, name='chart'),
 ]
--- dashboardreguser/views.py
+++ dashboardreguser/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def dashboarduser(request):
     return render(request, 'dashboard.html')
+
+
+def chart(request):
+    return render(request, 'chart.html')
