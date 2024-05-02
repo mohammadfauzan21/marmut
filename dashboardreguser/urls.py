1d4aa11faece64fad3f9ff9be8c737dffc18a97b
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-05-02 23:10:51 +0700

    fix chart

--- dashboardreguser/urls.py
+++ dashboardreguser/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from dashboardreguser.views import dashboarduser,chart
+from dashboardreguser.views import dashboarduser, kelolaplaylist, playsong, userplaylist, chart
 
 app_name = 'dashboardreguser'
 urlpatterns = [
     path('', dashboarduser, name='dashboarduser'),
-    path('chart', chart, name='chart'),
+    path('kelolaplaylist/', kelolaplaylist, name='kelolaplaylist'),
+    path('playsong/', playsong, name='playsong'),
+    path('userplaylist/', userplaylist, name='userplaylist'),
+    path('chart/', chart, name='chart'),
 ]
--- dashboardreguser/views.py
+++ dashboardreguser/views.py
@@ -4,6 +4,15 @@ from django.shortcuts import render
 def dashboarduser(request):
     return render(request, 'dashboard.html')
 
+def kelolaplaylist(request):
+    return render(request, 'kelolaplaylist.html')
+
+def playsong(request):
+    return render(request, 'playsong.html')
+
+def userplaylist(request):
+    return render(request, 'userplaylist.html')
+
 
 def chart(request):
     return render(request, 'chart.html')
