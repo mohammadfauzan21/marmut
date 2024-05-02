31c5c9c87defc86f930a2bc7835dca9aa7626b3e
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-05-02 22:30:23 +0700

    Bagian Fauzan Selesai

--- dashboardreguser/urls.py
+++ dashboardreguser/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from dashboardreguser.views import dashboarduser
+from dashboardreguser.views import dashboarduser, kelolaplaylist, playsong, userplaylist
 
 app_name = 'dashboardreguser'
 urlpatterns = [
     path('', dashboarduser, name='dashboarduser'),
+    path('kelolaplaylist/', kelolaplaylist, name='kelolaplaylist'),
+    path('playsong/', playsong, name='playsong'),
+    path('userplaylist/', userplaylist, name='userplaylist'),
 ]
--- dashboardreguser/views.py
+++ dashboardreguser/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def dashboarduser(request):
     return render(request, 'dashboard.html')
+
+def kelolaplaylist(request):
+    return render(request, 'kelolaplaylist.html')
+
+def playsong(request):
+    return render(request, 'playsong.html')
+
+def userplaylist(request):
+    return render(request, 'userplaylist.html')
