fa8ed558018303cb3ef8c090bbe87151ca9e350f
sarahnazly <sarahnazlya533@gmail.com>
2024-05-02 13:49:21 +0700

    add create podcast feature

--- dashboardpodcaster/urls.py
+++ dashboardpodcaster/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from dashboardpodcaster.views import podcaster
+from dashboardpodcaster.views import podcaster, episodes
 
-app_name = 'dasboard_podcaster'
+app_name = 'dashboard'
 urlpatterns = [
     path('', podcaster, name='podcaster'),
+    path('episodes/', episodes, name='episodes'),
 ]
