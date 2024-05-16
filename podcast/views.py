0899646b252501ae3e5bf5efa445978c0b44e32e
sarahnazly <sarahnazlya533@gmail.com>
2024-05-16 10:19:23 +0700

    Fitur R Podcast Done

--- podcast/views.py
+++ podcast/views.py
@@ -32,17 +32,24 @@ def detail_podcast(request, id_konten):
         with connection.cursor() as cursor:
             # Fetch podcast details including the year
             cursor.execute("""
-                SELECT K.judul, array_agg(G.genre), A.nama, K.durasi, K.tanggal_rilis, K.tahun
+                SELECT K.id, K.judul, array_agg(G.genre), A.nama, K.durasi, K.tanggal_rilis, K.tahun
                 FROM KONTEN K
                 JOIN PODCAST P ON K.id = P.id_konten
                 JOIN PODCASTER Po ON P.email_podcaster = Po.email
                 JOIN AKUN A ON Po.email = A.email
                 JOIN GENRE G ON K.id = G.id_konten
-                WHERE K.id = %s
-                GROUP BY K.judul, A.nama, K.durasi, K.tanggal_rilis, K.tahun
+                WHERE K.id = %s       
+                GROUP BY K.id, K.judul, A.nama, K.durasi, K.tanggal_rilis, K.tahun
             """, [id_konten])
 
             podcast = cursor.fetchone()
+            
+
+            print(podcast)
+
+            # Check if podcast is None
+            if podcast is None:
+                return HttpResponseNotFound("Podcast not found")
 
             # Fetch episode details
             cursor.execute("""
@@ -56,17 +63,16 @@ def detail_podcast(request, id_konten):
 
         # Calculate total episodes and total duration
         total_episodes = len(episodes)
-        total_duration = sum(episode[2] for episode in episodes)
 
         # Prepare context data for rendering
         context = {
-            'judul_podcast': podcast[0],
-            'genres': podcast[1],
-            'podcaster': podcast[2],
+            'judul_podcast': podcast[1],
+            'genres': podcast[2],
+            'podcaster': podcast[3],
             'total_episodes': total_episodes,
-            'total_duration': format_durasi(podcast[3]),
-            'tanggal_rilis': podcast[4],
-            'tahun_podcast': podcast[5],  # Add the year to the context
+            'total_duration': format_durasi(podcast[4]),
+            'tanggal_rilis': podcast[5],
+            'tahun_podcast': podcast[6],  # Add the year to the context
             'episodes': [{
                 'judul': episode[0],
                 'deskripsi': episode[1],
@@ -74,7 +80,7 @@ def detail_podcast(request, id_konten):
                 'tanggal_rilis': episode[3]
             } for episode in episodes]
         }
-
+        
         return render(request, 'detail_podcast.html', context)
     except OperationalError:
         return HttpResponseNotFound("Database connection error")
