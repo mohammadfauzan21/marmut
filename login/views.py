38e4f59b7db0db9220df40a7919ac3a2980e9611
muhammadfawwazarshadsaid <muhammad.fawwaz22@ui.ac.id>
2024-05-17 12:55:52 +0700

    fix login label

--- login/views.py
+++ login/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.contrib import messages
 
 def loginkonten(request):
     if request.method == 'POST':
@@ -13,52 +14,60 @@ def loginkonten(request):
             cursor.execute("SELECT * FROM akun WHERE email = %s AND password = %s", [user_email, user_password])
             akun_exists = cursor.fetchone()
 
-            
             # Kirim data ke template tergantung pada apa yang ditemukan
             if akun_exists:
-                # Cek apakah verified PUSING BGT YAALLAH
+                # Cek apakah verified
                 cursor.execute("SELECT is_verified FROM akun WHERE email = %s", [user_email])
                 is_verified = cursor.fetchone()[0]
 
                 # Kalau gak verified, berarti ya otomatis reguler user
                 if not is_verified:
+                    request.session['user_email'] = user_email
+                    request.session['user_type'] = 'unverified'
                     return redirect('dashboardreguser:dashboarduser')
                 # Jika verified, maka cek email terdaftar di mana aja
                 else:
                     # Cek email apakah terdaftar sebagai artist/songwriter, terdaftar sebagai podcaster, atau justru terdaftar sebagai artist/songwriter dan podcaster
+                    cursor.execute("SELECT * FROM artist WHERE email_akun = %s", [user_email])
+                    is_artist = cursor.fetchone()
+                    cursor.execute("SELECT * FROM songwriter WHERE email_akun = %s", [user_email])
+                    is_songwriter = cursor.fetchone()
+                    cursor.execute("SELECT * FROM podcaster WHERE email = %s", [user_email])
+                    is_podcaster = cursor.fetchone()
 
-                        # Check if the email is registered as an artist
-                        cursor.execute("SELECT * FROM artist WHERE email_akun = %s", [user_email])
-                        is_artist = cursor.fetchone()
-                        # Check if the email is registered as a songwriter
-                        cursor.execute("SELECT * FROM songwriter WHERE email_akun = %s", [user_email])
-                        is_songwriter = cursor.fetchone()
-                        # Check if the email is registered as a podcaster
-                        cursor.execute("SELECT * FROM podcaster WHERE email = %s", [user_email])
-                        is_podcaster = cursor.fetchone()
+                    user_roles = []
 
-                        # registered as either artist or songwriter or both
-                        if (is_artist or is_songwriter) and (is_artist and is_songwriter):
-                            return redirect('dashboardartist:homepageartist')  # Redirect to the artist/songwriter dashboard
-                        # INI BISA DIBUKA YAH. registered as both artist/songwriter and podcaster
-                        # elif is_artist and is_songwriter and is_podcaster:
-                        #     return redirect('dashboardmix:homepagemix')  # Redirect to a common dashboard page for both types
-                        # INI BISA DIBUKA YAH. registered as podcaster only
-                        # elif is_podcaster:
-                        #     return redirect('dashboardpodcaster:homepagepodcaster')  # Redirect to the podcaster dashboard
+                    # Menyimpan informasi peran-peran pengguna dalam session
+                    if is_artist:
+                        user_roles.append('artist')
+                    if is_songwriter:
+                        user_roles.append('songwriter')
+                    if is_podcaster:
+                        user_roles.append('podcaster')
 
-                
+                    request.session['user_email'] = user_email
+                    request.session['user_type'] = 'verified'
+                    request.session['user_roles'] = user_roles
+
+                    # Redirect ke halaman yang sesuai
+                    if is_artist and is_songwriter:
+                        return redirect('dashboardartist:homepageartist')
+                    elif is_podcaster:
+                        return redirect('dashboardpodcaster:homepagepodcaster')
+                    else:
+                        return redirect('dashboardreguser:dashboarduser')
+
             else:
-                # Jika akun tidak ditemukan, coba cek kali aja akun Label
+                # Cek ada gak akun label
                 cursor.execute("SELECT * FROM label WHERE email = %s AND password = %s", [user_email, user_password])
                 label_exist = cursor.fetchone()
                 if label_exist:
+                    request.session['user_email'] = user_email
                     return redirect('dashboardlabel:homepagelabel')
                 else:
-                    # Tidak ditemukan di akun atau label
+                # Jika akun tidak ditemukan, tampilkan pesan kesalahan
                     error_message = "Username atau password belum terdaftar."
                     return render(request, 'login.html', {'error_message': error_message})
-                
 
     else:
         # Jika bukan metode POST, kembalikan halaman login kosong
