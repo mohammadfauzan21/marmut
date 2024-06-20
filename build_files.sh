012f32195ca51595254cda5d3ce7462c06558211
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-06-20 23:49:45 +0700

    coba lagi vercel perubahan di build_files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,9 @@
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/Scripts/activate
+
 # build_files.sh
 python3.9 -m pip install -r requirements.txt
 
