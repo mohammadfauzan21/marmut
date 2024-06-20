cc165f27eafbbcad6a165465dbf5efc0a32cd57c
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-06-21 00:43:04 +0700

    terakhir untuk malam ini deploy vercel

--- build_files.sh
+++ build_files.sh
@@ -1,12 +1,13 @@
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
+python3.9 -m venv env
 
 # activate the virtual environment
-source venv/Scripts/activate
+env/Scripts/activate
 
 # build_files.sh
 python3.9 -m pip install -r requirements.txt
 
 # make migrations
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
