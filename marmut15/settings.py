39b3714b95d2dbf895a2de0852383b618eebf218
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-06-20 21:04:54 +0700

    Mau deploy vercel

--- marmut15/settings.py
+++ marmut15/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^sv_l_8jf*$3h1ja!$lg=o4u6ojt_wk+(&h223n1b@h4!mu$8_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 # Static Files
 
@@ -147,6 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- marmut15/urls.py
+++ marmut15/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,6 @@ urlpatterns = [
     path('dashboard/', include('dashboarduser.urls')),
     path('dashboardlabel/', include('dashboardlabel.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
