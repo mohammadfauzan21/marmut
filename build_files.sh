74207cb7f9dd07224e11a0ee4eb5d52e56ca2e19
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-06-21 16:12:08 +0700

    coba lagi dah

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,18 @@
+echo "BUILD START"
+
 # create a virtual environment named 'venv' if it doesn't already exist
 python3.9 -m venv venv
 
 # activate the virtual environment
-venv/bin/activate
+source venv/bin/activate
 
-# build_files.sh
+# install all deps in the venv
 pip install -r requirements.txt
 
-# make migrations
-python3.9 manage.py makemigrations
-python3.9 manage.py migrate 
+# collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
+
+echo "BUILD END"
+
+# [optional] Start the application here 
+# python manage.py runserver
