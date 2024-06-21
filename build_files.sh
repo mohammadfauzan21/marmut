d39d7a35b7cdb4f9cc013d372d1787f49e06cb58
Fauzan <mohammad.fauzan21@ui.ac.id>
2024-06-21 16:10:46 +0700

    coba lagi

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,13 @@
 # create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv env
+python3.9 -m venv venv
 
 # activate the virtual environment
-env/Scripts/activate
+venv/bin/activate
 
 # build_files.sh
-python3.9 pip install -r requirements.txt
+pip install -r requirements.txt
 
 # make migrations
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python manage.py collectstatic --noinput
